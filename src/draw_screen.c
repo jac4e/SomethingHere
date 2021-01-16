2077a1b7ea206d719fa96c32040caf2cca1211b4
sam popham <1greenpanda@gmail.com>
2021-01-16 14:34:23 -0700

    opengl now links

---
+++ src/draw_screen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GLFW_INCLUDE_NONE // prevent OpenGL include conflicts with GLAD
+
+#include <GLFW/glfw3.h>
+#include "../lib/glad/glad.h"
+
+int create_screen()
+{
+	return 0;
+}
+
+int draw_screen(char **tiles, int dims[2])
+{
+	return 0;
+}
