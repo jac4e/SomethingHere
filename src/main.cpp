8e156a1374257ffc2b8595ef481f14da0feebaec
Jacques Fourie <jacques@fourie.ca>
2021-01-18 17:05:41 -0700

    Created simple simulation code in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 #include <iostream>
 #include <vector>
@@ -20,6 +20,51 @@ Map map(100, 100, 0, 1000, 0, 10);
 int time = 0;
 int deaths = 0;
 int main() {
+    int highest = 0;
+    int genHighest = 0;
+    int generation = 0;
+    std::vector<Agent> population = generateAgents(20, 100);
+    printf("Starting...\n");
+    while (1) {
+        deaths = 0;
+        time = 0;
+        printf("Placing agents...\n");
+        map.placeAgents(population);
+        while (1) {
+            //wait for user input to move to next frame
+            std::cin.ignore();
+            map.print();
+            for (int i = 0; i < population.size(); i++) {
+                if (population[i].energyStorage <= 0) {
+                    //std::cout << "dead agent\n";
+                }
+                //printMap();
+                control(population[i], population);
+            }
+            if (deaths == population.size()) {
+                for (int i = 0; i < population.size(); i++) {
+                    calculateLitness(population[i]);
+                    
+                    if (population[i].energyUsed > genHighest) {
+                        genHighest = population[i].litness;
+                    }
+                }
+                if (genHighest > highest)
+                {
+                    highest = genHighest;
+                }
+                printf("Generation %d\n - genHighest %d - Overall %d\n", generation, genHighest, highest);
+                break;
+            }
+            ++time;
+        }
+        printf("Reproducing agents...\n");
+        reproducePopulation(population);
+        ++generation;
+        genHighest = 0;
+        map.clear();
+    }
+
     // struct display_data dat;
     // struct tick_vars tick;
 
